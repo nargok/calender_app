2dd760684688a8a366dce67c620077b0b0425c48
nargok <nargok@gmail.com>
2018-09-03 11:52:48 +0900

    曜日出力を実装

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import WEEKDAYS from './components/weekDays'
+
+class Calender extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello</h1>
+        <table>
+          <WEEKDAYS />
+        </table>
+      </React.Fragment>
+    )
+  }
+}
+
+
+ReactDOM.render(<Calender />, document.getElementById('root'));
 registerServiceWorker();
