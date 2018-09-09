4d7092a99f90c88e9d2caf28dca51fcfcd17a170
Ryoichi Gokan <nargok@gmail.com>
2018-09-09 18:33:14 +0900

    カレンダーテーブルのうち、今日日付のものに背景をつける

--- src/dateList/dateList.js
+++ src/dateList/dateList.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+import classnames from 'classnames';
 
+import CustomDate from '../components/customDate';
 import './dateList.css';
 
  class DateList extends Component {
+  isTody = (date) => {
+    const today = new CustomDate(new Date());
+    return date.formatDate() === today.formatDate();
+  }
+
   render() {
     const dateList = _.map(this.props.dateList, (date, index) => {
       return <td
                key={index}
-               className="date"
+               className={classnames("date", { "today" : this.isTody(date) })}
                onClick={() => this.props.selectDate(date)}>{date.date}
              </td>
     })
